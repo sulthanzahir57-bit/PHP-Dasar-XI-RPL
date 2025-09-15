60edcd1f445ef039fb29016aff18480013f6c97a
sulthanzahir57-bit <sulthanzahir57@gmail.com>
2025-09-15 13:36:36 +0700

    variabel
    PHP Dasar Variabel

---
+++ variabel/latihan 2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variabel Latihan 2</title>
+</head>
+<body>
+    <?php
+    $nama="Sulthan Zahir Arief";
+    $kelas="XI RPL";
+    $alamat="Jl.Bagus Yabin no 56,Cigadung,Kab,subang";
+
+    echo "Nama : $nama";
+    echo "<br>";
+    echo "Kelas : $kelas";
+    echo "<br>";
+    echo "Alamat : $alamat";
+      ?>
+</body>
+</html>
